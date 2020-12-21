2f3cc12f17d1b79f9525fb8d6b0c888f33bd654b
MICHAEL4560 <71989878+MICHAEL4560@users.noreply.github.com>
2020-12-21 01:54:01 -0500

    Update config.php
    fix external database conn

--- includes/config.php
+++ includes/config.php
@@ -7,10 +7,18 @@
 
 
     //remote database
-      $host = 'remotemysql.com';
-      $db = '0SSv31YWLC';
-      $user= '0SSv31YWLC';
-      $password='Hc5ulZwdyQ';
-      $charset = 'utf8mb4';
+    //$host = 'remotemysql.com';
+    //$db = '0SSv31YWLC';
+   // $user= '0SSv31YWLC';
+    //$password='Hc5ulZwdyQ';
+   // $charset = 'utf8mb4';
+
+
+
+      define( 'DB_HOST', 'remotemysql.com' );          // Set database host
+      define( 'DB_USER', '0SSv31YWLC' );             // Set database user
+      define( 'DB_PASS', 'Hc5ulZwdyQ' );             // Set database password
+      define( 'DB_NAME', '0SSv31YWLC' );        // Set database name
+
 
 ?>
